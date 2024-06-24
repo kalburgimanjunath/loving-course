b9a50bd04d764e54585e633b38c5f5239130b8bd
manjunath k <manjunathkalburgi85@gmail.com>
2024-06-24 14:36:12 +0000

    tailwind added

--- src/components/lessonList.tsx
+++ src/components/lessonList.tsx
@@ -1,27 +1,32 @@
 import React from "react";
+import Link from "react-router-dom";
 export default function LessonList({ lessons, title }) {
   const Pages = ({ pages }) => {
     return (
-      <div>
+      <div className="p-2 border">
         {pages &&
           pages.map((item) => {
-            return <div>{item.title}</div>;
+            return <Link>{item.title}</Link>;
           })}
       </div>
     );
   };
   return (
     <div>
-      <h3 className="text-blue-500">{title}</h3>
-      {lessons &&
-        lessons.map((item, index) => {
-          return (
-            <div className="font-bold text-2xlg" key={item.id}>
-              {item.title}
-              <Pages pages={item.pages} />
-            </div>
-          );
-        })}
+      <h3 className="text-white font-bold bg-blue-500 p-2">{title}</h3>
+      <ol className="list-decimal p-2 ml-4">
+        {lessons &&
+          lessons.map((item, index) => {
+            return (
+              <li className="text-2xlg capitalize font-bold" key={item.id}>
+                <div className="font-bold rounded-t-lg p-2 uppercase">
+                  {item.title}
+                </div>
+                <Pages pages={item.pages} />
+              </li>
+            );
+          })}
+      </ol>
     </div>
   );
 }
